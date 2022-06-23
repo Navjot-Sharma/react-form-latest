bb029bdac6929562de8a61d077465703086f6a2e
Navjot Sharma <navi00sharma@gmail.com>
2022-06-24 02:25:13 +0530

    readme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.scss';
+import {Form, TextInput, SubmitButton} from 'react-form-latest';
 
 function App(props) {
   return (
-   <></>
+   <>
+    <Form>
+      <TextInput 
+        field='firstName'
+        label='First Name'
+        required
+      />
+      <TextInput 
+        field='lastName'
+        label='Last Name'
+      />
+
+      <SubmitButton>Submit</SubmitButton>
+    </Form>
+   </>
   );
 }
 
